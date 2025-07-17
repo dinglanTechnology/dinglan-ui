9c5170733449b672b0e6b6527fadb9afe5b5690f
wy <wuying_code@163.com>
2025-07-17 11:43:37 +0800

    feat(数字): 新增金额类型

--- src/components/InputNumber/InputNumber.tsx
+++ src/components/InputNumber/InputNumber.tsx
@@ -8,21 +8,20 @@ export default function DlInputNumber(props: TDlInputNumberProps) {
   const { useType, value, onChange, formatter, parser, ...restProps } = props;
 
   const moneyFormatter = (value: number | string | undefined) => {
-    if (value === undefined || value === null) return "";
-    const num = typeof value === "string" ? parseFloat(value) : value;
-    return `${num}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    if (!value) return "";
+    const numValue = typeof value === "string" ? parseFloat(value) : value;
+    return `${numValue.toLocaleString("zh-CN")}`;
   };
 
   const moneyParser = (displayValue: string | undefined) => {
     if (!displayValue) return "";
-    const value = displayValue.replace(/¥\s?|(,*)/g, "");
-    return parseFloat(value) || "";
+    return displayValue.replace(/,/g, "");
   };
 
   return (
     <InputNumber
       {...restProps}
-      formatter={useType === "money" ? moneyFormatter : formatter}
+      formatter={useType ? moneyFormatter : formatter}
       parser={useType === "money" ? moneyParser : parser}
       controls={false}
       value={value}
